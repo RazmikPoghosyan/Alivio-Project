ae9aaf255f4b9453f10861348298ed8c0bfa7b2d
RazmikPoghosyan <razpoghosyaan@gmail.com>
2023-11-29 23:06:46 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+
+mobileNavButton.addEventListener('click', function() {
+    mobileNavIcon.classList.toggle('active');
+    mobileNav.classList.toggle('active');
+    document.body.classList.toggle('no-scrool');
+})
+
+// video
+
+const videoBtn = document.querySelector('#video-story-btn');
+const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+const videoOverlay = document.querySelector('#video-story-overlay');
+const videoFile = document.querySelector('#video-story');
+videoBtn.addEventListener('click', function () {
+
+	function toggleOverlay(event){
+		if (event.type === 'mouseleave') {
+			videoOverlay.classList.add('hidden');
+		} else {
+			videoOverlay.classList.remove('hidden');
+		}
+	}
+
+	if (videoFile.paused) {
+		videoFile.play();
+		videoBtnIcon.src = './img/story/pause-white.svg';
+
+		videoOverlay.onmouseleave = toggleOverlay;
+		videoOverlay.onmouseenter = toggleOverlay;
+
+	} else {
+		videoFile.pause();
+		videoBtnIcon.src = './img/story/play-white.svg';
+		videoOverlay.onmouseleave = null;
+		videoOverlay.onmouseenter = null;
+
+	}
+
+})
